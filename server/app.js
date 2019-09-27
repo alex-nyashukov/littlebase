ca49aa0e7a1ac9f01dd4b86bc546a50825e697c6
alex-nyashukov <alex-nyashukov@gmail.com>
2019-09-27 23:30:24 +0300

    stable

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 
-var fs = require('fs');
-var Bus = require('./models').Bus
-var Driver = require('./models').Driver
+// var fs = require('fs');
+// var Bus = require('./models').Bus
+// var Driver = require('./models').Driver
 
 const app = express()
 mongoose.set('useFindAndModify', false);
@@ -19,20 +19,20 @@ mongoose.connect('mongodb://localhost/base', {useNewUrlParser: true, useUnifiedT
    //       model.save()
    //    })
    // })
-   fs.readFile('server/db_drivers.txt', 'utf8', function(err, contents) {
-      contents.split('\n').forEach((value) => {
-         let data = value.split(';')
+   // fs.readFile('server/db_drivers.txt', 'utf8', function(err, contents) {
+   //    contents.split('\n').forEach((value) => {
+   //       let data = value.split(';')
 
-         let oldGraphic = JSON.parse(data[3])
-         let newGraphic = { date: oldGraphic.startDate.substr(0,10), items: oldGraphic.graphicItems.map((value) => ({ status: value.Status, days: ''+value.NumberOfDays }))}
+   //       let oldGraphic = JSON.parse(data[3])
+   //       let newGraphic = { date: oldGraphic.startDate.substr(0,10), items: oldGraphic.graphicItems.map((value) => ({ status: value.Status, days: ''+value.NumberOfDays }))}
          
-         Bus.findOne({busnumber: data[2]}, (err, res) => {
-            let document = { tabnumber: data[0], name: data[1], bus: res._id, graphic: newGraphic }
-            let model = new Driver(document)
-            model.save()
-         })
-      })
-   })
+   //       Bus.findOne({busnumber: data[2]}, (err, res) => {
+   //          let document = { tabnumber: data[0], name: data[1], bus: res._id, graphic: newGraphic }
+   //          let model = new Driver(document)
+   //          model.save()
+   //       })
+   //    })
+   // })
 });
 
 app.use(cookieParser())
